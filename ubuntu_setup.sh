6fe5fda79b7dad762693457d87de1c1107da9a4f
Darren Prentice <prenticedarren@gmail.com>
2020-02-21 14:45:23 -0600

    --

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6,14 +6,17 @@
 
 # pip3 install aubio==0.4.9 ffmpeg-normalize==1.15.6
 
+# git clone https://github.com/dpren/pitch-extractor.git
+# cd pitch-extractor
+
 # stack setup
 # stack build
 
-## apt update && apt full-upgrade -y && apt install -y haskell-stack python3 python3-pip ffmpeg youtube-dl && pip3 install aubio==0.4.9 ffmpeg-normalize==1.15.6
 
 #==========================================
 # Only required if you have < 1.5GB memory:
 #==========================================
+cd
 sudo mkdir -v /var/cache/swap
 cd /var/cache/swap
 sudo dd if=/dev/zero of=swapfile bs=1K count=4M
